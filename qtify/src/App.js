4df3766402019b0c6b7e07092c6eef886a6b4a88
Raj Janrao <rajjanrao@Rajs-MacBook-Air.local>
2025-09-15 00:19:05 +0530

    Check commit

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,15 +1,37 @@
+import React, { use, useEffect, useState } from 'react';
+import { StyledEngineProvider } from '@mui/material/styles';
+import { Outlet, outlet } from 'react-router-dom';
+import { fetchFilters, fetchNewAlbums, fetchSongs, fetchTopAlbums } from './Component/api';
 import './App.css';
 import Hero from './Component/Hero';
 import Navbar from './Component/Navbar';
 
 function App() {
+  const [data, setData] = useState({});
+
+  const loadData = (key, source) => {
+    source().then((fetchedData) => {
+      setData((prevData) => ({
+        ...prevData,
+        [key]: fetchedData,
+      }));
+    });
+  }
+
+  useEffect(() => {
+    loadData('newAlbums', fetchNewAlbums);
+    loadData('topAlums', fetchTopAlbums);
+    loadData('songs', fetchSongs);
+    loadData('genres', fetchFilters);
+  }, []); 
+
+  const { newAlbums = [], topAlums = [], songs = [], genres = [] } = data;
   return (
     <div className="App">
-      <Navbar />
-      <Hero />
-      <header className="App-header">
-        
-      </header>
+      <StyledEngineProvider injectFirst>
+        <Navbar searchData={[...newAlbums, ...topAlums, ...songs]} />
+        <Outlet context={{ data: { newAlbums, topAlums, songs, genres } }} />
+      </StyledEngineProvider>
     </div>
   );
 }
